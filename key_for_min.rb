843eadd78df58944e47272f8ec7bfd9345b37d19
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-03 18:20:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash == false
+  if name_hash.length == 0 
+    return nil
   end
   lowest_value = 1000
   lowest_key = ""
